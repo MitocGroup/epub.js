80324c37126c08a367f4c3c65667bfea74adbe42
AlexanderC <alexander.moldova@gmail.com>
2016-04-29 13:10:46 +0300

    Add Highlighter to exports

--- src/epub.js
+++ src/epub.js
@@ -1,12 +1,13 @@
 var Book = require('./book');
 var EpubCFI = require('./epubcfi');
+var Highlighter = require('./highlighter');
 
 function ePub(_url) {
 	return new Book(_url);
 };
 
 ePub.VERSION = "0.3.0";
-
 ePub.CFI = EpubCFI;
+ePub.Highlighter = Highlighter;
 
 module.exports = ePub;
